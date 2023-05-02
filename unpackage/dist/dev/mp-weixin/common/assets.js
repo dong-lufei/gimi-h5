29e8e0b98a032a588b4ea38b85b6513d4a26bea6
董路飞 <hb.donglufei@qq.com>
2023-05-03 00:07:06 +0800

    firstCommit

--- unpackage/dist/dev/mp-weixin/common/assets.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-const _imports_0$1 = "/static/missing-face.png";
-const _imports_0 = "/static/icon_search.png";
-const _imports_1 = "/static/icon_application.png";
-const _imports_2 = "/static/icon_purchase.png";
-exports._imports_0 = _imports_0$1;
-exports._imports_0$1 = _imports_0;
-exports._imports_1 = _imports_1;
-exports._imports_2 = _imports_2;
